73260de0794a4d3bbd7e5b3f5dc653c284cd18dc
Miz-Yam <gyakusyuboo@gmail.com>
2021-02-18 14:40:38 +0000

    Set up controller, routing and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'books#welcome'
+  resources :books, :except => :new
 end
