29f45fb39da14d8843cec4188b13d531f06efc54
Miz-Yam <gyakusyuboo@gmail.com>
2021-02-18 15:54:50 +0000

    add posting function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,24 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
   end
 
   def show
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
